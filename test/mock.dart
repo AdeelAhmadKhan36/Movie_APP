eac5dd42d1e359b93750df4c587018d96ce461db
Adeel Ahmad Khan <97823437+AdeelAhmadKhan36@users.noreply.github.com>
2024-08-17 16:20:37 +0500

    Update

--- test/mock.dart
+++
@@ -1,7 +0,0 @@
-import 'package:api_app/data/services/api_service.dart';
-import 'package:mockito/mockito.dart';
-import 'package:http/http.dart' as http;
-
-// Create mock classes
-class MockClient extends Mock implements http.Client {}
-class MockApiService extends Mock implements ApiService {}
